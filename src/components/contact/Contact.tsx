4a7925b45b072a794c08da98416c0ed3a72daf82
Daisuke <d.suke.kida@gmail.com>
2023-10-19 00:49:56 +0900

    🚧 コンタクトセクションの開発中

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,7 +4,42 @@ import styles from "./Contact.module.scss";
 function Contact() {
   return (
     <div className={styles.contact}>
-      <div>contact</div>
+      <div className="wrapper">
+        {/* sectionのタイトル */}
+        <div className={styles.sectionTitle}>
+          <h2>CONTACT</h2>
+        </div>
+
+        {/* sectionの中身 */}
+        <div className={styles.sectionContents}>
+          <div className={styles.contactTextArea}>
+            <p className={styles.contactTextJa}>
+              どのようなことでもかまいませんので、お気軽にお問い合わせください
+            </p>
+            <p className={styles.contactTextEn}>
+              Please feel free to contact us for any inquiries or questions, no
+              matter what the topic may be!
+            </p>
+          </div>
+          <form className={styles.contactForm}>
+            <div className={styles.formItem}>
+              <label>your name</label>
+              <input type="text" />
+            </div>
+            <div className={styles.formItem}>
+              <label>email</label>
+              <input type="text" />
+            </div>
+            <div className={styles.formItem}>
+              <label>お問い合わせ内容</label>
+              <textarea name="" id=""></textarea>
+            </div>
+            <div>
+              <button type="submit">so-shin!</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
