fc221f075e665f893d4f0cfb3255dac7ffd3f4fd
Daisuke <d.suke.kida@gmail.com>
2023-10-31 01:33:00 +0900

    💄 skillコンポーネントの追加

---
+++ src/components/skill/Skill.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Skill.module.scss";
+
+function Skill() {
+  return (
+    <div className={styles.skill}>
+      <div>skill</div>
+    </div>
+  );
+}
+
+export default Skill;
