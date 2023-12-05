b1ce135aa43a1939b85429a3c7a641b289ccb798
Daisuke <d.suke.kida@gmail.com>
2023-12-06 00:53:43 +0900

    fix/text

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -6,7 +6,7 @@ function History() {
     <div className={styles.history}>
       <div className="wrapper">
         <div className={styles.sectionTitle}>
-          <h2>SKILL</h2>
+          <h2>HISTORY</h2>
         </div>
         <div className={styles.sectionContents}></div>
       </div>
